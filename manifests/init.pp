4f35ffcccebed7e12113bdd4a471938e03cdfe50
Geoff Williams <geoff@geoffwilliams.me.uk>
2017-05-31 11:10:06 +1000

    initial

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,48 +1,13 @@
-# Class: windows_timezone
-# ===========================
+# Windows_timezone
 #
-# Full description of class windows_timezone here.
-#
-# Parameters
-# ----------
-#
-# Document parameters here.
-#
-# * `sample parameter`
-# Explanation of what this parameter affects and what it defaults to.
-# e.g. "Specify one or more upstream ntp servers as an array."
-#
-# Variables
-# ----------
-#
-# Here you should define a list of variables that this module would require.
-#
-# * `sample variable`
-#  Explanation of how this variable affects the function of this class and if
-#  it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#  External Node Classifier as a comma separated list of hostnames." (Note,
-#  global variables should be avoided in favor of class parameters as
-#  of Puppet 2.6.)
-#
-# Examples
-# --------
-#
-# @example
-#    class { 'windows_timezone':
-#      servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#    }
-#
-# Authors
-# -------
-#
-# Author Name <author@domain.com>
-#
-# Copyright
-# ---------
-#
-# Copyright 2017 Your name here, unless otherwise noted.
-#
-class windows_timezone {
-
-
+# Set the windows timezone using the tzutil command
+# @see https://stackoverflow.com/a/16656265/3441106
+class windows_timezone(
+    $tz = "UTC",
+) {
+  exec { "set the windows timezone":
+    command => "tzutil /s \"${tz}\"",
+    unless  => "findstr \"${tz}\"",
+    path    => 'c:\windows',
+  }
 }
