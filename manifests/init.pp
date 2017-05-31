881a174437e191dca5205c8473000b8148468000
Geoff Williams <geoff@geoffwilliams.me.uk>
2017-05-31 11:58:16 +1000

    initial release

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,12 +2,14 @@
 #
 # Set the windows timezone using the tzutil command
 # @see https://stackoverflow.com/a/16656265/3441106
+#
+# @param tz The timezone to set this node to use
 class windows_timezone(
     $tz = "UTC",
 ) {
   exec { "set the windows timezone":
     command => "tzutil /s \"${tz}\"",
     unless  => "findstr \"${tz}\"",
-    path    => 'c:\windows',
+    path    => 'c:\windows\system32',
   }
 }
