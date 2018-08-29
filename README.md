c437989f11199a863abd98e5e89b493e404c6637
Geoff Williams <geoff@geoffwilliams.me.uk>
2018-08-29 16:55:22 +1000

    use puppet datatypes, don't set UTC as default any more (breaking change), upgrade pdqtest, switch to reference docs, release

--- README.md
+++ README.md
@@ -5,7 +5,6 @@
 
 1. [Description](#description)
 1. [Usage - Configuration options and additional functionality](#usage)
-1. [Reference - An under-the-hood peek at what the module is doing and how](#reference)
 1. [Limitations - OS compatibility, etc.](#limitations)
 1. [Development - Guide for contributing to the module](#development)
 
@@ -16,14 +15,6 @@ Set the Windows timezone using Puppet
 ## Usage
 See reference and examples
 
-## Reference
-[generated documentation](https://rawgit.com/GeoffWilliams/puppet-windows_timezone/master/doc/index.html).
-
-Reference documentation is generated directly from source code using [puppet-strings](https://github.com/puppetlabs/puppet-strings).  You may regenerate the documentation by running:
-
-```shell
-bundle exec puppet strings
-```
 
 ## Limitations
 * Not supported by Puppet, Inc.
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,15 @@
-# Windows_timezone
+# @summary Set the windows timezone using the `tzutil` command
 #
-# Set the windows timezone using the tzutil command
+# You must specify the timezone to use in the correct format to the `tz` parameter
+# otherwise this class will take no action.
+#
+# @example Setting the timezone
+#   class { "windows_timezone":
+#     tz => "New Zealand Standard Time",
+#   }
+#
+# @see https://github.com/GeoffWilliams/puppet-windows_timezone/blob/master/doc/windows_timezones.txt
+#
 # @see https://stackoverflow.com/a/16656265/3441106
 #
 # @param tz The timezone to set this node to use.  You must only the names
@@ -9,17 +18,19 @@
 #   inside this module. The value to use for `tz` is the _second_ line of each
 #   entry.
 class windows_timezone(
-    $tz = "UTC",
+    Optional[String] $tz = undef
 ) {
 
-  # must invoke windows command shell directly if we want to use OS pipes, see
-  # https://projects.puppetlabs.com/issues/21656
-  # 
-  # tzutil sets the SYSTEM timezone for all users at once. Must use /C to 
-  # fully match input otherwise the exec will not fire correctly
-  exec { "set the windows timezone":
-    command => "tzutil /s \"${tz}\"",
-    unless  => "cmd /C tzutil /g | findstr /C:\"${tz}\"",
-    path    => 'c:\windows\system32',
+  if $tz {
+    # must invoke windows command shell directly if we want to use OS pipes, see
+    # https://projects.puppetlabs.com/issues/21656
+    #
+    # tzutil sets the SYSTEM timezone for all users at once. Must use /C to
+    # fully match input otherwise the exec will not fire correctly
+    exec { "set the windows timezone":
+      command => "tzutil /s \"${tz}\"",
+      unless  => "cmd /C tzutil /g | findstr /C:\"${tz}\"",
+      path    => 'c:\windows\system32',
+    }
   }
 }
